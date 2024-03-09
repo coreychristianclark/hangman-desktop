659d66cbdc998fcf6fe542f8b162e417a5137dc9
Corey Clark <coreychristianclark@gmail.com>
2024-03-09 18:41:13 -0500

    Keyboard created

--- hangman-desktop/src/App.tsx
+++ hangman-desktop/src/App.tsx
@@ -28,7 +28,9 @@ function App() {
 
       <HangmanDrawing />
       <HangmanWord />
-      <Keyboard />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+        </div>
     </div>
   );
 }
--- hangman-desktop/src/HangmanWord.tsx
+++ hangman-desktop/src/HangmanWord.tsx
@@ -1,3 +1,35 @@
 export function HangmanWord() {
-    
+  const word = "text";
+  const guessedLetters = ["t","e", "g"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map(
+        (
+          letter,
+          index // This will split the word into each letter and give us the index of each letter so we can work with the characters individually.
+        ) => (
+          <span style={{ borderBottom: "0.1em solid black" }} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span> // Gives us our bottom platform that will serve as a 'highlight' for each character.
+        )
+      )}
+    </div>
+  );
 }
