0cb35065390f630dd4ec1569f6e44ae003da34c2
Corey Clark <coreychristianclark@gmail.com>
2024-03-09 14:25:37 -0500

    Created a basic layout for App.tsx. Moving on to creating the components

--- hangman-desktop/src/App.tsx
+++ hangman-desktop/src/App.tsx
@@ -1,11 +1,33 @@
-
-import './App.css'
+import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]; // This is giving us a value between 0 and 1, multiplying it by the length of our word list (so, 0 and the length of our list), and then rounding the value down. This is how our words will be generated.
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]); // Type set to string array.
+
+  console.log(wordToGuess);
 
   return (
-<h1>hi</h1>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem", // Gap set between the keys.
+        margin: "0 auto", // Centers everything.
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
